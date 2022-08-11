2d91b525885464a0b7eb1b199ca26976e094b7c6
Silen Locatelli <88315530+SilenLoc@users.noreply.github.com>
2022-08-11 09:09:14 +0200

    adding documentation

--- easy_safe_pack/src/lib.rs
+++ easy_safe_pack/src/lib.rs
@@ -4,6 +4,25 @@ pub use crate::mapenv::disk_pers::{create_or_load_map_env, MapEnv};
 extern crate core;
 
 
+//! yes it is not easy_save since this crate also wants to be safe to use while saving I called
+//! it easy_safe
+//!
+//! This is a crate that should make it possible to save and load strings with keys from disk
+//! It works as a map and actually wraps a map
+//! It keeps the ownership model as you know it
+//! but you may always call ```create_or_load_map_env("somename")```
+//! at any place in your code, with the same name it was first being called
+//! This makes it possible to leak data through places where the data should not be
+//! as you can imagine, you should be careful to not trying to use multiple environments concurrently
+//!
+//! ```
+//! use easy_safe::{create_or_load_map_env, MapEnv};
+//!
+//! let mut  map_env: MapEnv = create_or_load_map_env("somename");
+//! map_env.put("somekey".to_string(), "somevalue".to_string());
+//! let value = map_env.get("somekey".to_string()).unwrap();
+//! assert_eq!(value, "somevalue");
+//! ```
 
 #[cfg(test)]
 mod tests {
--- easy_safe_pack/src/mapenv.rs
+++ easy_safe_pack/src/mapenv.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{Read, Write};
+
 use serde::{Deserialize, Serialize};
 
 pub struct PersistenceEnv {
@@ -33,6 +34,7 @@ impl PersistenceEnv {
     }
 }
 
+/// creates a "environment" on the disk, it just wraps writing and reading from a file
 pub fn env_default_at(name: &str) -> PersistenceEnv {
     PersistenceEnv {
         name: name.parse().unwrap()
@@ -42,6 +44,7 @@ pub fn env_default_at(name: &str) -> PersistenceEnv {
 
 pub mod disk_pers {
     use std::collections::HashMap;
+
     use crate::mapenv::{env_default_at, MapWrapper, PersistenceEnv};
 
     pub struct MapEnv {
@@ -50,6 +53,9 @@ pub mod disk_pers {
     }
 
     impl MapEnv {
+        /// put will first try to load the newest file,
+        /// puts the value into the in memory map and then saves the map to disk
+        /// also return the value
         pub fn put(&mut self, key: String, value: String) -> Option<String> {
             self.update_inner();
             let value = self.inner.inner.insert(key, value);
@@ -57,11 +63,14 @@ pub mod disk_pers {
             value
         }
 
+        /// deletes the value in the in memory map and then saves the map to disk
+        /// and also returns the value
         pub fn delete(&mut self, key: String) {
             self.inner.inner.remove(&*key);
             self.update_outer();
         }
 
+        /// get trys to load the map from disk and then attempts to get the value from the in memory map
         pub fn get(&mut self, key: String) -> Option<&String> {
             self.update_inner();
             self.inner.inner.get(&*key)
@@ -81,10 +90,29 @@ pub mod disk_pers {
             self.env.save_to_local(&*serialized.unwrap())
         }
 
-        pub(crate) fn get_name (&self) -> &String {&self.env.name}
+        /// returns the name of the inner environment
+        pub(crate) fn get_name(&self) -> &String { &self.env.name }
     }
 
 
+    /// creates a new map environment with the specified name
+    /// the name corresponds to the file saved and loaded in your filesystem
+    /// If the file is already there it will load from that file
+    ///
+    /// This means you can always come back and access your file if you call it with the right name
+    ///
+    /// # Examples
+    ///```
+    ///  use easy_safe::{create_or_load_map_env, MapEnv};
+    ///  let mut  map_env: MapEnv = create_or_load_map_env("somename");
+    ///  map_env.put("somekey".to_string(), "somevalue".to_string());
+    ///  let value = map_env.get("somekey".to_string()).unwrap();
+    ///  assert_eq!(value, "somevalue");
+    ///
+    ///  let mut  same_file_map_env: MapEnv = create_or_load_map_env("somename");
+    ///  let also_the_value = same_file_map_env.get("somekey".to_string()).unwrap();
+    ///  assert_eq!(value, "also_the_value");
+    ///```
     pub fn create_or_load_map_env(name: &str) -> MapEnv {
         let pers_env = env_default_at(name);
 
@@ -99,6 +127,9 @@ pub mod disk_pers {
     }
 }
 
+/// the wrapping struct to the map, this is what will be saved as json on the disk
+/// this is mostly to not break interfaces and it should make it always possible
+/// to load old files if needed
 #[derive(Serialize, Deserialize)]
 struct MapWrapper {
     inner: HashMap<String, String>,

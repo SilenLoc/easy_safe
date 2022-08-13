a58affe15b64433d49d53cfece85c65d76df9f96
SilenLoc <private@locatelli.ch>
2022-08-13 17:41:57 +0200

    add new envr

--- easy_safe_pack/src/lib.rs
+++ easy_safe_pack/src/lib.rs
@@ -24,13 +24,14 @@
 
 /// map environment
 mod mapenv;
-pub use crate::mapenv::disk_pers::{create_or_load_map_env, MapEnv};
+pub use crate::mapenv::disk_pers::{create_or_load_map_env, MapEnv, create_or_load_late_save_map_env, LateSaveMapEnv};
 
 extern crate core;
 
 #[cfg(test)]
 mod tests {
     use std::fs;
+    use crate::{create_or_load_late_save_map_env, LateSaveMapEnv};
     use crate::mapenv::disk_pers::{create_or_load_map_env, MapEnv};
 
     fn clear_files(name: &str) {
@@ -62,6 +63,41 @@ mod tests {
         assert_eq!(value, None);
         clear_files("name4");
     }
+
+    #[test]
+    fn create_env_and_check_name_with_late_save() {
+        let map_env: LateSaveMapEnv = create_or_load_late_save_map_env("name6");
+        assert_eq!(map_env.get_name(), "name6");
+        clear_files("name6");
+    }
+
+    #[test]
+    fn create_env_put_and_get_with_late_save() {
+        let mut map_env: LateSaveMapEnv = create_or_load_late_save_map_env("name7");
+        map_env.put("somekey", "somevalue");
+        map_env.save();
+
+        let mut new_map_env_instance: LateSaveMapEnv = create_or_load_late_save_map_env("name7");
+
+        let value = new_map_env_instance.get("somekey").unwrap();
+        assert_eq!(value, "somevalue");
+        clear_files("name7");
+    }
+
+    #[test]
+    fn create_env_put_and_get_with_late_save_without_saving_to_disk() {
+        let mut map_env: LateSaveMapEnv = create_or_load_late_save_map_env("name8");
+        map_env.put("somekey", "somevalue");
+        /// map_env.save();
+
+        let mut new_map_env_instance: LateSaveMapEnv = create_or_load_late_save_map_env("name8");
+
+        let value = new_map_env_instance.get("somekey");
+        assert_eq!(value, None);
+        clear_files("name8");
+    }
+
+
 }
 
 
--- easy_safe_pack/src/mapenv.rs
+++ easy_safe_pack/src/mapenv.rs
@@ -54,7 +54,7 @@ pub mod disk_pers {
     impl MapEnv {
         /// put will first try to load the newest file,
         /// puts the value into the in memory map and then saves the map to disk
-        /// also return the value
+        /// also returns the value
         pub fn put(&mut self, key: &str, value: &str) -> Option<String> {
             self.update_inner();
             let value = self.inner.inner.insert(
@@ -67,7 +67,6 @@ pub mod disk_pers {
 
         /// deletes the value in the in memory map and then saves the map to disk
         /// and also returns the value
-
         pub fn delete(&mut self, key: &str) {
             self.inner.inner.remove(key);
             self.update_outer();
@@ -124,6 +123,87 @@ pub mod disk_pers {
         };
         pers_env.init_env(&serde_json::to_string(&map_wrapper).unwrap());
         MapEnv {
+            inner: map_wrapper,
+            env: pers_env,
+        }
+    }
+    pub struct LateSaveMapEnv {
+        inner: MapWrapper,
+        env: PersistenceEnv,
+    }
+
+    impl LateSaveMapEnv {
+
+        /// put will first try to load the newest file,
+        /// puts the value into the in memory map
+        /// also returns the value
+        pub fn put(&mut self, key: &str, value: &str) -> Option<String> {
+            self.update_inner();
+            let value = self.inner.inner.insert(
+                key.parse().unwrap(),
+                value.parse().unwrap(),
+            );
+            value
+        }
+
+        /// deletes the value in the in memory map
+        /// and also returns the value
+        pub fn delete(&mut self, key: &str) {
+            self.update_inner();
+            self.inner.inner.remove(key);
+        }
+
+        /// get trys to load the map from disk and then attempts to get the value from the in memory map
+        pub fn get(&mut self, key: &str) -> Option<&String> {
+            self.update_inner();
+            self.inner.inner.get(&*key)
+        }
+
+        fn update_inner(&mut self) {
+            let from_file = self.env.load_from_local();
+
+            let map: MapWrapper = serde_json::from_str(&*from_file).unwrap();
+            for (key, value) in &map.inner {
+                self.inner.inner.insert(key.clone(), value.clone());
+            }
+        }
+
+        pub fn save(&mut self) {
+            let serialized = serde_json::to_string(&self.inner);
+            self.env.save_to_local(&*serialized.unwrap())
+        }
+
+        /// returns the name of the inner environment
+        pub(crate) fn get_name(&self) -> &String { &self.env.name }
+
+    }
+
+    /// creates a new map environment with the specified name that does not save everytime
+    /// an operation is done on the map
+    /// the name corresponds to the file saved and loaded in your filesystem
+    /// If the file is already there it will load from that file
+    ///
+    /// This means you can always come back and access your file if you call it with the right name
+    ///
+    /// # Examples
+    ///```
+    ///  use easy_safe::{create_or_load_late_save_map_env, LateSaveMapEnv};
+    ///  let mut  map_env: LateSaveMapEnv = create_or_load_late_save_map_env("somename");
+    ///  map_env.put("somekey", "somevalue");
+    ///  map_env.save();
+    ///
+    ///  let mut  same_file_map_env: LateSaveMapEnv = create_or_load_late_save_map_env("somename");
+    ///  let also_the_value = same_file_map_env.get("somekey").unwrap();
+    ///  assert_eq!(also_the_value, "somevalue");
+    ///```
+    pub fn create_or_load_late_save_map_env(name: &str) -> LateSaveMapEnv {
+        let pers_env = env_default_at(name);
+
+        let map_wrapper = MapWrapper {
+            inner: HashMap::new()
+        };
+        pers_env.init_env(&serde_json::to_string(&map_wrapper).unwrap());
+        LateSaveMapEnv {
             inner: map_wrapper,
             env: pers_env,
         }
